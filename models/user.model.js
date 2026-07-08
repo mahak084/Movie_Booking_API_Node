28473a8328031b9f847be775f5c8750e8d4756cf
mahak084 <matelemahak@gmail.com>
2026-07-08 12:51:27 +0530

    Feature:Input password comparison logicin signin

--- models/user.model.js
+++ models/user.model.js
@@ -45,7 +45,11 @@ userSchema.pre('save',async function(next){
     next();
 })
 
-
+userSchema.methods.isValidPassword=async(plainPassword)=>{
+    const currentUser=this;
+    const compare=await bcrypt.compare(plainPassword,currentUser.password);
+    return compare;
+}
 
 const User=mongoose.model('User',userSchema);
 module.exports=User;
