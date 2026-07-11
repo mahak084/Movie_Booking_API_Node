7b925cf3db8b37bbeb6565b92579c225fe549709
mahak084 <matelemahak@gmail.com>
2026-07-11 12:00:47 +0530

    Feature: Added autherization in theatre and movie routes

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,25 +1,32 @@
 const MovieController=require('../controllers/movie.controller')
 const MovieMiddleware=require('../middlewares/movie.middleware')
+const authMiddleware=require('../middlewares/auth.middleware')
 
 
 const routes=(app)=>{
     app.post('/mba/api/v1/movies',
+        authMiddleware.isAuthenticated,
+        authMiddleware.isAdminOrClient,
         MovieMiddleware.validateMovieCreateRequest,
         MovieController.createMovie);
 
     app.delete('mba/api/v1/movies/:id',
-        
+        authMiddleware.isAuthenticated,
+        authMiddleware.isAdminOrClient,
         MovieController.deleteMovie);
 
     app.get('/mba/api/v1/movies/:id',
-       
         MovieController.getMovie);
 
     app.put('mba/api/v1/movies/:id',
+        authMiddleware.isAuthenticated,
+        authMiddleware.isAdminOrClient,
         MovieMiddleware.validateMovieCreateRequest,
         MovieController.updateMovie);
 
     app.patch('mba/api/v1/movies/:id',
+        authMiddleware.isAuthenticated,
+        authMiddleware.isAdminOrClient,
         MovieController.updateMovie);
 
     app.get('/mba/api/v1/movies',
